048b444ee78f3d6b27e4ddfcff5e02051b8e1f53
Fangolin <jakubkowalczyk0192@o2.pl>
2024-07-11 16:25:46 +0200

    Basically finished. A bit wonky but functional.

--- react-tic-tac-toe/src/components/Board.tsx
+++ react-tic-tac-toe/src/components/Board.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Cell from "./Cell";
 
 class CellInfo {
@@ -10,6 +10,20 @@ class CellInfo {
   constructor(id: number, state: string) {
     this.id = id;
     this.state = state;
+  }
+}
+
+class WinningLineParams {
+  x1: string;
+  y1: string;
+  x2: string;
+  y2: string;
+
+  constructor(x1: string, y1: string, x2: string, y2: string) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
   }
 }
 
@@ -35,12 +49,80 @@ function* getNextMoveGenerator(validMoves: string[]): Generator<string> {
 
 function Board() {
   const [currentState, setCurrentState] = useState(getInitialBoardState);
-  const nextMove = useRef(getNextMoveGenerator(["X", "O"])).current;
+  const getNextMove = useRef(getNextMoveGenerator(["X", "O"])).current;
   const [currentMove, setCurrentMove] = useState("");
+  const [previousMove, setPreviousMove] = useState("");
+  const [win, setWin] = useState<WinningLineParams | null>(null);
+
+  useEffect(() => setCurrentMove(getNextMove.next().value), []);
 
   useEffect(() => {
-    setCurrentMove(nextMove.next().value);
-  }, []);
+    let winningLine = checkWin(previousMove);
+    if (winningLine != null) {
+      setWin(winningLine);
+    }
+  }, [previousMove]);
+
+  function checkWin(playerSymbol: string) {
+    if (playerSymbol === "") {
+      return null;
+    }
+
+    console.log("Check win for " + playerSymbol);
+
+    if (
+      currentState[0].state === playerSymbol &&
+      currentState[1].state === playerSymbol &&
+      currentState[2].state === playerSymbol
+    ) {
+      return new WinningLineParams("10%", "25%", "90%", "25%");
+    } else if (
+      currentState[3].state === playerSymbol &&
+      currentState[4].state === playerSymbol &&
+      currentState[5].state === playerSymbol
+    ) {
+      return new WinningLineParams("10%", "50%", "90%", "50%");
+    } else if (
+      currentState[6].state === playerSymbol &&
+      currentState[7].state === playerSymbol &&
+      currentState[8].state === playerSymbol
+    ) {
+      return new WinningLineParams("10%", "75%", "90%", "75%");
+    } else if (
+      currentState[0].state === playerSymbol &&
+      currentState[3].state === playerSymbol &&
+      currentState[6].state === playerSymbol
+    ) {
+      return new WinningLineParams("25%", "10%", "25%", "90%");
+    } else if (
+      currentState[1].state === playerSymbol &&
+      currentState[4].state === playerSymbol &&
+      currentState[7].state === playerSymbol
+    ) {
+      return new WinningLineParams("50%", "10%", "50%", "90%");
+    } else if (
+      currentState[2].state === playerSymbol &&
+      currentState[5].state === playerSymbol &&
+      currentState[8].state === playerSymbol
+    ) {
+      return new WinningLineParams("75%", "10%", "75%", "90%");
+    } else if (
+      currentState[0].state === playerSymbol &&
+      currentState[4].state === playerSymbol &&
+      currentState[8].state === playerSymbol
+    ) {
+      return new WinningLineParams("10%", "10%", "90%", "90%");
+    } else if (
+      currentState[2].state === playerSymbol &&
+      currentState[4].state === playerSymbol &&
+      currentState[6].state === playerSymbol
+    ) {
+      return new WinningLineParams("90%", "10%", "10%", "90%");
+    } else {
+      console.log("No win found");
+      return null;
+    }
+  }
 
   function makeMove(cellId: number) {
     setCurrentState((currentState) =>
@@ -53,7 +135,8 @@ function Board() {
       })
     );
 
-    setCurrentMove(nextMove.next().value);
+    setPreviousMove(currentMove);
+    setCurrentMove(getNextMove.next().value);
   }
 
   const cells = currentState.map((cellInfo) => (
@@ -64,6 +147,7 @@ function Board() {
     />
   ));
 
+  // wait for starting move to be determined
   if (!currentMove) {
     return <div>Loading...</div>;
   }
@@ -71,18 +155,26 @@ function Board() {
   return (
     <div className="flex flex-col w-full items-center">
       <div className="grid grid-cols-3 grid-rows-3 border-2 w-32">
-        <svg className="absolute visible">
-          <line
-            x1="10%"
-            y1="10%"
-            x2={300}
-            y2={300}
-            style={{ stroke: "white", strokeWidth: "3px" }}
-          />
-        </svg>
+        {win && (
+          <svg className="absolute w-32 aspect-square visible">
+            (
+            <line
+              x1={win.x1}
+              y1={win.y1}
+              x2={win.x2}
+              y2={win.y2}
+              style={{ stroke: "white", strokeWidth: "3px" }}
+            />
+            )
+          </svg>
+        )}
         {cells}
       </div>
-      <span className="my-1 font-semibold">Current move: {currentMove}</span>
+      <span className="my-1 font-semibold">
+        {win == null
+          ? "Current move: " + currentMove
+          : "Winner: " + previousMove}
+      </span>
     </div>
   );
 }
--- react-tic-tac-toe/src/components/Cell.tsx
+++ react-tic-tac-toe/src/components/Cell.tsx
@@ -1,5 +1,3 @@
-import { ReactNode } from "react";
-
 function Cell({ state, moveHandler }: any) {
   return (
     <button
