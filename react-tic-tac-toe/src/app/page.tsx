543de87e9cd622500a9c1924c2ea854c033a7654
Fangolin <jakubkowalczyk0192@o2.pl>
2024-07-11 14:30:43 +0200

    just WIP, possibly some restructuring required

--- react-tic-tac-toe/src/app/page.tsx
+++ react-tic-tac-toe/src/app/page.tsx
@@ -2,9 +2,9 @@ import Board from "@/components/Board";
 
 export default function Game() {
   return (
-    <div className="flex items-center flex-col ">
+    <div className="flex items-center flex-col">
       <div className="text-3xl font-bold my-3">Tic-Tac-Toe</div>
-      <Board />
+      <Board/>
     </div>
   );
 }
--- react-tic-tac-toe/src/components/Board.tsx
+++ react-tic-tac-toe/src/components/Board.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import { useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import Cell from "./Cell";
 
 class CellInfo {
@@ -13,40 +13,75 @@ class CellInfo {
   }
 }
 
-function getInitialBoardState(){
+function getInitialBoardState() {
   const initialState: CellInfo[] = [];
   const initialValue: string = "";
 
   for (let i = 0; i < 9; i++) {
-      initialState.push(new CellInfo(i, initialValue));
+    initialState.push(new CellInfo(i, initialValue));
   }
 
   return initialState;
 }
 
+function* getNextMoveGenerator(validMoves: string[]): Generator<string> {
+  let index = Math.floor(Math.random() * 2);
+
+  while (true) {
+    yield validMoves[index];
+    index = ++index % 2;
+  }
+}
+
 function Board() {
   const [currentState, setCurrentState] = useState(getInitialBoardState);
-  const currentMove: string = "X"
+  const nextMove = useRef(getNextMoveGenerator(["X", "O"])).current;
+  const [currentMove, setCurrentMove] = useState("");
 
-  function makeMove(cellId: number){
-    console.log(`Move id: ${cellId}`);
+  useEffect(() => {
+    setCurrentMove(nextMove.next().value);
+  }, []);
 
-    setCurrentState(currentState.map(cell => {
-      if (cell.id === cellId){
-        return { ...cell, state: currentMove };
-      }
+  function makeMove(cellId: number) {
+    setCurrentState((currentState) =>
+      currentState.map((cell) => {
+        if (cell.id === cellId) {
+          return { ...cell, state: currentMove };
+        }
 
-      return cell;
-    }))
+        return cell;
+      })
+    );
+
+    setCurrentMove(nextMove.next().value);
   }
 
   const cells = currentState.map((cellInfo) => (
-    <Cell key={cellInfo.id} state={cellInfo.state} moveHandler={() => makeMove(cellInfo.id)}/>
+    <Cell
+      key={cellInfo.id}
+      state={cellInfo.state}
+      moveHandler={() => makeMove(cellInfo.id)}
+    />
   ));
 
+  if (!currentMove) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="flex flex-col">
-      <div className="grid grid-cols-3 grid-rows-3 border-2">{cells}</div>
+    <div className="flex flex-col w-full items-center">
+      <div className="grid grid-cols-3 grid-rows-3 border-2 w-32">
+        <svg className="absolute visible">
+          <line
+            x1="10%"
+            y1="10%"
+            x2={300}
+            y2={300}
+            style={{ stroke: "white", strokeWidth: "3px" }}
+          />
+        </svg>
+        {cells}
+      </div>
       <span className="my-1 font-semibold">Current move: {currentMove}</span>
     </div>
   );
