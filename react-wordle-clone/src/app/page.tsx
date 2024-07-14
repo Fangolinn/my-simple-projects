dbe62e8c5a017db0e5d67c8a2c1c5374bfcca31b
Fangolin <jakubkowalczyk0192@o2.pl>
2024-07-15 00:49:39 +0200

    Some small progress, mostly experimenting.

--- react-wordle-clone/src/app/page.tsx
+++ react-wordle-clone/src/app/page.tsx
@@ -1,5 +1,24 @@
+"use client";
+
+import wordleValidWords from "../resources/wordle_valid_words.json";
+import { useState } from "react";
+
 export default function Root() {
-  return (
-    <div></div>
-  );
+  let [message, setMessage] = useState("dupa");
+
+  function handleWord(e: any) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    const word = formData.get("word")?.toString();
+
+    if (word && wordleValidWords.includes(word)) {
+      setMessage("Valid word.");
+    } else {
+      setMessage("Invalid word.");
+    }
+  }
+
+  return <><p>{message}</p><form onSubmit={handleWord}><input name="word" type="text" className="bg-black"/><button type="submit">Submit</button></form></>;
 }
