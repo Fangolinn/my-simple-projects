b4f6c3e03a4a47496fccafbe3c66988719cfd681
Fangolinn <jakubkowalczyk0192@o2.pl>
2024-08-07 17:25:04 +0200

    WIP (experimenting)

--- react-simple-todo/src/app/page.tsx
+++ react-simple-todo/src/app/page.tsx
@@ -1,3 +1,55 @@
+"use client";
+
+import React, { FormEvent, useEffect, useState } from "react";
+
+type ToDo = {
+  completed: boolean;
+  content: string;
+};
+
+let testToDos: ToDo[] = [
+  { completed: false, content: "aaa" },
+  { completed: true, content: "bbb" },
+];
+
 export default function Home() {
-  return <p>test</p>;
+  const [todos, setTodos] = useState<ToDo[]>(testToDos);
+
+  const handleNewTodo = (e: FormEvent<HTMLFormElement>): void => {
+    console.log("submit");
+
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    const newTodo: ToDo = { completed: false, content: formData.get("newTodo") as string };
+
+    setTodos([...todos, newTodo]);
+  };
+
+  const todosJSX = todos.map((todo) => (
+    <li key={todo.content}>
+      <input
+        type="checkbox"
+        defaultChecked={todo.completed}
+        id={todo.content}
+      />
+      <label htmlFor={todo.content}>{todo.content}</label>
+    </li>
+  ));
+
+  return (
+    <>
+      <ul>{todosJSX}</ul>
+      <form onSubmit={handleNewTodo}>
+        <input
+          type="text"
+          name="newTodo"
+          id="newTodo"
+          className="text-black"
+        />
+        <input type="submit" value="Submit"/>
+      </form>
+    </>
+  );
 }
